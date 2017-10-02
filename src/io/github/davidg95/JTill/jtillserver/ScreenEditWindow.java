68304a4072db46de067fc90c5a0710fcf25105fe
David <David@DAVID-PC.JG.Computers.Grant>
2017-10-02 11:19:23 +0100

    db

--- src/io/github/davidg95/JTill/jtillserver/ScreenEditWindow.java
+++ src/io/github/davidg95/JTill/jtillserver/ScreenEditWindow.java
@@ -24,7 +24,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.BorderFactory;
-import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
