5daf7690e7d77804040a6bd045bd1526507e57f1
David <David@DAVID-PC.JG.Computers.Grant>
2018-01-04 15:11:22 +0000

    WasteStockWindow now uses a table model

--- src/io/github/davidg95/JTill/jtillserver/WasteStockWindow.java
+++ src/io/github/davidg95/JTill/jtillserver/WasteStockWindow.java
@@ -17,6 +17,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,7 +35,10 @@ import javax.swing.SpinnerDateModel;
 import javax.swing.SwingUtilities;
 import javax.swing.event.InternalFrameAdapter;
 import javax.swing.event.InternalFrameEvent;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 /**
  *
@@ -43,16 +47,15 @@ import javax.swing.table.DefaultTableModel;
 public class WasteStockWindow extends javax.swing.JInternalFrame {
 
     private final DataConnect dc;
-    private List<WasteItem> wasteItems;
-    private final DefaultTableModel model;
     private Date date;
+
+    private final MyModel model;
 
     /**
      * Creates new form WasteStockWindow
      */
     public WasteStockWindow() {
         this.dc = GUI.gui.dc;
-        wasteItems = new ArrayList<>();
         initComponents();
         setTitle("Waste Stock");
         super.setClosable(true);
@@ -80,9 +83,8 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
         Date t = cb.getTime();
         pickDate.setDate(new Date());
         timeSpin.setValue(t);
-        model = (DefaultTableModel) tblProducts.getModel();
+        model = new MyModel();
         tblProducts.setModel(model);
-        model.setRowCount(0);
         init();
         txtBarcode.requestFocus();
     }
@@ -100,21 +102,24 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
     }
 
     private void init() {
+        tblProducts.getColumnModel().getColumn(0).setMaxWidth(40);
+        tblProducts.getColumnModel().getColumn(2).setMaxWidth(40);
+        tblProducts.getColumnModel().getColumn(0).setMinWidth(40);
+        tblProducts.getColumnModel().getColumn(2).setMinWidth(40);
         tblProducts.setSelectionModel(new ForcedListSelectionModel());
         this.addInternalFrameListener(new InternalFrameAdapter() {
             @Override
             public void internalFrameClosing(InternalFrameEvent e) {
-                if (!wasteItems.isEmpty()) {
+                if (!model.getItems().isEmpty()) {
                     int res = JOptionPane.showInternalConfirmDialog(WasteStockWindow.this, "Do you want to save the current report?", "Save", JOptionPane.YES_NO_OPTION);
                     if (res == JOptionPane.YES_OPTION) {
-                        GUI.gui.savedReports.put("WAS", wasteItems);
+                        GUI.gui.savedReports.put("WAS", model.getItems());
                     }
                 }
             }
         });
         if (GUI.gui.savedReports.containsKey("WAS")) {
-            wasteItems = GUI.gui.savedReports.get("WAS");
-            updateTable();
+            model.setItems(GUI.gui.savedReports.get("WAS"));
             GUI.gui.savedReports.remove("WAS");
         }
         InputMap im = tblProducts.getInputMap(JTable.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
@@ -127,47 +132,19 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
             @Override
             public void actionPerformed(ActionEvent evt) {
                 final int index = tblProducts.getSelectedRow();
-                final WasteItem it = wasteItems.get(index);
+                final WasteItem it = model.getWasteItem(index);
                 final Product p = it.getProduct();
                 if (index == -1) {
                     return;
                 }
                 if (JOptionPane.showInternalConfirmDialog(WasteStockWindow.this, "Are you sure you want to remove this item?\n" + p.getName(), "Stock Item", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-                    wasteItems.remove(index);
-                    updateTable();
+                    model.removeWasteItem(index);
                 }
             }
         });
     }
 
-    private void updateTable() {
-        String symbol = "";
-        try {
-            symbol = dc.getSetting("CURRENCY_SYMBOL");
-        } catch (IOException ex) {
-            Logger.getLogger(WasteStockWindow.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        model.setRowCount(0);
-        BigDecimal val = BigDecimal.ZERO;
-        for (WasteItem wi : wasteItems) {
-            Object[] row = new Object[]{wi.getProduct().getId(), wi.getProduct().getLongName(), wi.getQuantity(), symbol + new DecimalFormat("0.00").format(wi.getTotalValue()), wi.getReason().getReason()};
-            model.addRow(row);
-            val = val.add(wi.getTotalValue());
-        }
-        if (val == BigDecimal.ZERO) {
-            lblValue.setText("Total Value: £0.00");
-        } else {
-            lblValue.setText("Total Value: £" + new DecimalFormat("0.00").format(val));
-        }
-        if (wasteItems.isEmpty()) {
-            btnWaste.setEnabled(false);
-        } else {
-            btnWaste.setEnabled(true);
-        }
-    }
-
     private Date getSelectedDate() {
-
         long d = pickDate.getDate().getTime();
         long t = ((Date) timeSpin.getValue()).getTime();
         date = new Date(d + t);
@@ -176,6 +153,146 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
         c.set(Calendar.HOUR_OF_DAY, c.get(Calendar.HOUR_OF_DAY) + 1);
         date = c.getTime();
         return date;
+    }
+
+    private class MyModel implements TableModel {
+
+        private List<WasteItem> items;
+        private final List<TableModelListener> listeners;
+
+        public MyModel() {
+            items = new LinkedList<>();
+            listeners = new LinkedList<>();
+        }
+
+        public void addWasteItem(WasteItem wi) {
+            items.add(wi);
+            alertAll();
+        }
+
+        public void removeWasteItem(int i) {
+            items.remove(i);
+            alertAll();
+        }
+
+        public WasteItem getWasteItem(int i) {
+            return items.get(i);
+        }
+
+        public void alertAll() {
+            for (TableModelListener l : listeners) {
+                l.tableChanged(new TableModelEvent(this));
+            }
+            BigDecimal val = BigDecimal.ZERO;
+            for (WasteItem wi : items) {
+                val = val.add(wi.getTotalValue());
+            }
+            if (val == BigDecimal.ZERO) {
+                lblValue.setText("Total Value: £0.00");
+            } else {
+                lblValue.setText("Total Value: £" + new DecimalFormat("0.00").format(val));
+            }
+        }
+
+        public void setItems(List<WasteItem> items) {
+            this.items = items;
+        }
+
+        public List<WasteItem> getItems() {
+            return items;
+        }
+
+        public void clear() {
+            items.clear();
+        }
+
+        @Override
+        public int getRowCount() {
+            return items.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 5;
+        }
+
+        @Override
+        public String getColumnName(int i) {
+            switch (i) {
+                case 0: {
+                    return "ID";
+                }
+                case 1: {
+                    return "Product";
+                }
+                case 2: {
+                    return "Qty.";
+                }
+                case 3: {
+                    return "Total Value";
+                }
+                case 4: {
+                    return "Reason";
+                }
+                default: {
+                    return "";
+                }
+            }
+        }
+
+        @Override
+        public Class<?> getColumnClass(int i) {
+            return Object.class;
+        }
+
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return columnIndex == 2;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int i) {
+            WasteItem item = items.get(rowIndex);
+            switch (i) {
+                case 0: {
+                    return item.getId();
+                }
+                case 1: {
+                    return item.getProduct().getLongName();
+                }
+                case 2: {
+                    return item.getQuantity();
+                }
+                case 3: {
+                    return item.getTotalValue();
+                }
+                case 4: {
+                    return item.getReason();
+                }
+                default: {
+                    return "";
+                }
+            }
+        }
+
+        @Override
+        public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+            WasteItem item = items.get(rowIndex);
+            if (columnIndex == 2) {
+                item.setQuantity(Integer.parseInt((String) aValue));
+            }
+            alertAll();
+        }
+
+        @Override
+        public void addTableModelListener(TableModelListener l) {
+            listeners.add(l);
+        }
+
+        @Override
+        public void removeTableModelListener(TableModelListener l) {
+            listeners.remove(l);
+        }
     }
 
     /**
@@ -416,22 +533,21 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
                 return;
             }
             WasteItem wi = new WasteItem(product, amount, wr, getSelectedDate());
-            wasteItems.add(wi);
+            model.addWasteItem(wi);
             txtBarcode.setText("");
-            updateTable();
         } else {
             JOptionPane.showInternalMessageDialog(this, "You must enter a number", "Waste Stock", JOptionPane.ERROR_MESSAGE);
         }
     }//GEN-LAST:event_btnAddProductActionPerformed
 
     private void btnWasteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnWasteActionPerformed
-        if (wasteItems.isEmpty()) {
+        if (model.getItems().isEmpty()) {
             return;
         }
         final ModalDialog mDialog = new ModalDialog(this, "Waste", "Processing...");
         final Runnable run = () -> {
             BigDecimal total = BigDecimal.ZERO;
-            for (WasteItem wi : wasteItems) {
+            for (WasteItem wi : model.getItems()) {
                 try {
                     Product product = dc.getProduct(wi.getProduct().getId());
                     product.removeStock(wi.getQuantity());
@@ -444,9 +560,8 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
             }
             total.setScale(2);
             try {
-                dc.addWasteReport(wasteItems);
-                model.setRowCount(0);
-                wasteItems.clear();
+                dc.addWasteReport(model.getItems());
+                model.clear();
                 lblValue.setText("Total: £0.00");
                 btnWaste.setEnabled(false);
                 mDialog.hide();
@@ -464,23 +579,7 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
 
     private void tblProductsMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblProductsMouseClicked
         final int index = tblProducts.getSelectedRow();
-        final WasteItem wi = wasteItems.get(index);
-        if (evt.getClickCount() == 2) {
-            if (!wasteItems.isEmpty()) {
-                String input = JOptionPane.showInternalInputDialog(WasteStockWindow.this, "Enter new quantity", "Waste Stock", JOptionPane.PLAIN_MESSAGE);
-                if (!Utilities.isNumber(input)) {
-                    JOptionPane.showInternalMessageDialog(WasteStockWindow.this, "A number must be entered", "Waste Stock", JOptionPane.ERROR_MESSAGE);
-                    return;
-                }
-                int val = Integer.parseInt(input);
-                if (val > 0) {
-                    wi.setQuantity(val);
-                    updateTable();
-                } else {
-                    JOptionPane.showInternalMessageDialog(WasteStockWindow.this, "Must be a value greater than zero", "Waste Stock", JOptionPane.WARNING_MESSAGE);
-                }
-            }
-        }
+        final WasteItem wi = model.getWasteItem(index);
 
         if (tblProducts.getSelectedRow() == -1) {
             return;
@@ -501,18 +600,18 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
                 int val = Integer.parseInt(input);
                 if (val > 0) {
                     wi.setQuantity(val);
-                    updateTable();
+                    model.alertAll();
                 } else {
                     JOptionPane.showInternalMessageDialog(WasteStockWindow.this, "Must be a value greater than zero", "Waste Stock", JOptionPane.WARNING_MESSAGE);
                 }
             });
-            changeReason.addActionListener((ActionEvent e) ->{
+            changeReason.addActionListener((ActionEvent e) -> {
                 WasteReason reason = WasteReasonSelectDialog.showDialog(this);
-                if(reason == null){
+                if (reason == null) {
                     return;
                 }
                 wi.setReason(reason);
-                updateTable();
+                model.alertAll();
             });
             item.addActionListener((ActionEvent e) -> {
                 final Product p = wi.getProduct();
@@ -520,8 +619,7 @@ public class WasteStockWindow extends javax.swing.JInternalFrame {
                     return;
                 }
                 if (JOptionPane.showInternalConfirmDialog(WasteStockWindow.this, "Are you sure you want to remove this item?\n" + p.getName(), "Stock Item", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-                    wasteItems.remove(index);
-                    updateTable();
+                    model.removeWasteItem(index);
                 }
             });
             menu.add(it);
