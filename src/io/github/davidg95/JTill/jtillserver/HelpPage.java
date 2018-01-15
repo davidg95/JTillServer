190ab8e2c859b7a16f681a17003d68a7006ef8b4
David <David@DAVID-PC.JG.Computers.Grant>
2018-01-15 17:12:37 +0000

    Work on Help Pages

--- src/io/github/davidg95/JTill/jtillserver/HelpPage.java
+++ src/io/github/davidg95/JTill/jtillserver/HelpPage.java
@@ -5,6 +5,12 @@
  */
 package io.github.davidg95.JTill.jtillserver;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author David
@@ -17,6 +23,13 @@ public class HelpPage extends javax.swing.JFrame {
     public HelpPage() {
         initComponents();
         this.setIconImage(GUI.icon);
+        URL url = getClass().getResource("/io/github/davidg95/JTill/resources/help/help_index.html");
+        try {
+            pane.setPage(url);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this, "Error loading help pages");
+            Logger.getGlobal().log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -29,7 +42,8 @@ public class HelpPage extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        pane = new javax.swing.JEditorPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Help");
@@ -37,30 +51,29 @@ public class HelpPage extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         jLabel1.setText("JTill Server");
 
-        jLabel2.setText("Help pages will go here");
+        jScrollPane1.setViewportView(pane);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(174, 174, 174)
-                        .addComponent(jLabel2)))
-                .addContainerGap(195, Short.MAX_VALUE))
+                        .addComponent(jLabel1)
+                        .addGap(0, 286, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(113, 113, 113)
-                .addComponent(jLabel2)
-                .addContainerGap(212, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 317, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -68,6 +81,7 @@ public class HelpPage extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JEditorPane pane;
     // End of variables declaration//GEN-END:variables
 }
